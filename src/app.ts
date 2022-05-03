cdaa8d905e5a3acae24ca6f308258fca6988999a
Antonio Anderson <hiroto.anderson@gmail.com>
2022-05-02 21:32:06 -0300

    nodejs-redis case

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import express from "express";
 import UserController from "./controllers/UserController";
+import redis from "./lib/cache";
 
 const app = express();
 
 app.get("/", (req, res) => res.send("server is running"));
 
 app.get("/users", UserController.find);
+
+app.get("/clear-cache", async (req, res) => {
+  await redis.del("users:all");
+
+  res.json({
+    ok: true,
+  });
+});
 
 app.listen(3000);
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import redis from "../lib/cache";
 
 const prisma = new PrismaClient();
 
 class UserController {
   static async find(req: Request, res: Response) {
     try {
+      const cacheKey = "users:all";
+
+      const cachedUsers = await redis.get(cacheKey);
       console.time("Find users");
+
+      if (cachedUsers) {
+        console.timeEnd("Find users");
+        return res.json(JSON.parse(cachedUsers));
+      }
+
       const users = await prisma.user.findMany();
 
       console.timeEnd("Find users");
+
+      await redis.set(cacheKey, JSON.stringify(users));
       return res.json(users);
     } catch (e) {
       console.log(e);
